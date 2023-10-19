44bcc05ca535a4faf312121903d5006da8449f55
Robert Fleming <robertefleming.ca@gmail.com>
2023-10-19 16:32:32 -0700

    Model Training and Evaluation

--- magic.py
+++ magic.py
@@ -13,6 +13,9 @@ from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 #################################################
 #     LOADING AND PREPROCESSING DATA
@@ -36,14 +39,14 @@ df.head()
 #################################################
 
 # plots several histograms color-coded based on the class of all the data
-for label in cols[:-1]:
-    plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
-    plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
-    plt.title(label)
-    plt.ylabel("Probability")
-    plt.xlabel(label)
-    plt.legend()
-    plt.show()
+# for label in cols[:-1]:
+#     plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+#     plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+#     plt.title(label)
+#     plt.ylabel("Probability")
+#     plt.xlabel(label)
+#     plt.legend()
+#     plt.show()
 
 #################################################
 #     DATA SPLITTING
@@ -78,9 +81,37 @@ train, X_train, y_train = scale_dataset(train, oversample=True)
 valid, X_valid, y_valid = scale_dataset(valid, oversample=False)
 test, X_test, y_test = scale_dataset(test, oversample=False)
 
+#################################################
+#       MODEL TRAINING AND EVALUATION
+#################################################
+
+# k-nearest neighbors
 knn_model = KNeighborsClassifier(n_neighbors=1)
 knn_model.fit(X_train, y_train)
 
 y_pred = knn_model.predict(X_test)
+#print(classification_report(y_test, y_pred))
 
+
+# naive bayes
+nb_model = GaussianNB()
+nb_model = nb_model.fit(X_train, y_train)
+
+y_pred = nb_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+
+# logistic regression
+lg_model = LogisticRegression()
+lg_model = lg_model.fit(X_train, y_train)
+
+y_pred = lg_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+
+# support vector classifier
+svm_model = SVC()
+svm_model = svm_model.fit(X_train, y_train)
+
+y_pred = svm_model.predict(X_test)
 print(classification_report(y_test, y_pred))
