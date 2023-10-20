d70d777f54212b486cb587a94d3b7b401deef45b
Robert Fleming <robertefleming.ca@gmail.com>
2023-10-20 09:18:59 -0700

    neural network model training and hyperparameter search

--- magic.py
+++ magic.py
@@ -16,6 +16,7 @@ from sklearn.metrics import classification_report
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+import tensorflow as tf
 
 #################################################
 #     LOADING AND PREPROCESSING DATA
@@ -114,4 +115,66 @@ svm_model = SVC()
 svm_model = svm_model.fit(X_train, y_train)
 
 y_pred = svm_model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+#################################################
+#   NEURAL NETWORK MODEL TRAINING AND HYPERPARAMETER SEARCH
+#################################################
+
+def plot_history(history):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    ax1.plot(history.history['loss'], label='loss')
+    ax1.plot(history.history['val_loss'], label='val_loss')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Binary crossentropy')
+    ax1.grid(True)
+
+    ax2.plot(history.history['accuracy'], label='accuracy')
+    ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Accuracy')
+    ax2.grid(True)
+
+    plt.show()
+
+
+def train_model(X_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs):
+    nn_model = tf.keras.Sequential([
+        tf.keras.layers.Dense(num_nodes, activation='relu', input_shape=(10,)),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(num_nodes, activation='relu'),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    nn_model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='binary_crossentropy',
+                     metrics=['accuracy'])
+    history = nn_model.fit(
+        X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0
+    )
+
+    return nn_model, history
+
+least_val_loss = float('inf')
+least_loss_model = None
+epochs=100
+for num_nodes in [16, 32, 64]:
+    for dropout_prob in [0, 0.2]:
+        for lr in [0.1, 0.005, 0.001]:
+            for batch_size in [32, 64, 128]:
+                print(f"{num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch size {batch_size}")
+                model, history = train_model(X_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs)
+                plot_history(history)
+                val_loss = model.evaluate(X_valid, y_valid)[0]
+                if val_loss < least_val_loss:
+                    least_val_loss = val_loss
+                    least_loss_model = model
+
+#################################################
+#   FINAL MODEL EVALUATION
+#################################################
+
+y_pred = least_loss_model.predict(X_test)
+y_pred = (y_pred > 0.5).astype(int).reshape(-1,)
+
 print(classification_report(y_test, y_pred))
